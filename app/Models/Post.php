b12940fb043f4c06f9b006cf0687c70a799210ba
poppots <takaya.chuo@gmail.com>
2024-01-20 11:28:35 +0900

    lev8-5完了

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,7 +14,7 @@ class Post extends Model
     'body',
     ];
     
-    public function getPaginateByLimit(int $limit_count = 5)
+    public function getPaginateByLimit(int $limit_count = 10)
 {
     return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
 }
